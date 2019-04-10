0bd81c87142ae6d43b36a9d6be005aa40c183fe9
martin.scocco <martin.scocco@santexgroup.com>
2019-04-10 15:31:00 -0300

    end chapter 5, boilerplate ready

--- server/controllers/authentication.js
+++ server/controllers/authentication.js
@@ -1,8 +1,25 @@
 const User = require('../models/user');
+const jwt = require('jwt-simple');
+const config = require('../config');
+
+function tokenForUser(user) {
+  const timestamp = new Date().getTime();
+  return jwt.encode({ sub: user.id, iat: timestamp }, config.secret);
+}
+
+exports.signin = function(req, res, next) {
+  // User has already had their email and password auth'd
+  // We just need to give then a token
+  res.send({ token: tokenForUser(req.user) });
+}
 
 exports.signup = function (req, res, next) {
   const email = req.body.email;
   const password = req.body.password;
+
+  if (!email || !password) {
+    return res.status(422).send({ error: 'you must provide email and password' });
+  }
 
   // See if a user with a given email exists
   User.findOne({ email:email }, function(err, existingUser) {
@@ -24,6 +41,6 @@ exports.signup = function (req, res, next) {
     });
   
     // Respond to request indicating the user was created
-    res.json({ success: true });
+    res.json({ token: tokenForUser(user) });
   });
 }
